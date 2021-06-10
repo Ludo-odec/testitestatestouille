27f0e7100e1ad6dbe361ea59c98a574ce643f06f
Ludovic Decuyper <62876890+Ludo-odec@users.noreply.github.com>
2021-06-10 14:29:13 +0200

    Update story.vue

--- src/views/story.vue
+++ src/views/story.vue
@@ -41,7 +41,7 @@ import ScrollTrigger from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 export default {
-  name: 'Stories',
+  name: 'Story',
   components: {
     StoryNavbar,
     FooterNavbar,
