277962adf30c3e891a4aec6ca2e5907be75dc223
Ludovic Decuyper <62876890+Ludo-odec@users.noreply.github.com>
2021-06-10 21:25:18 +0200

    oui

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@
       <component :is="Component" />
     </transition>
   </router-view>
-  <img class="background background--intro" src="@/assets/images/home/n1.jpg" alt="Image Norway">
-  <div class="warning">
-    <p>The mobile and tablet's size is coming soon !</p>
-    <p>I invite you to discover this site on Desktop for a better experience.</p>
-  </div>
   <audio
     class="myAudio"
     src="./audio/a1.wav"
@@ -30,19 +25,6 @@ export default {
 }
 .route-enter-active, .route-leave-active{
   transition: all ease 0.5s;
-}
-
-.warning{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  & p {
-    width: 41%;
-    font-size: 25.89px;
-  }
 }
 
 </style>
